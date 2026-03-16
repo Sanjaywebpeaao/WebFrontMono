7e872fb6393a1c2ce75ec6316b4368155b684a76
Your Name <webpeaao@gmail.com>
2026-03-16 11:14:55 +0530

    WebPeAao frontend Code

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>DevOps Practice Application</h1>
+
+      <div className="logo-container">
+        <img src="/logo.png" alt="DevOps Logo" className="logo" />
+      </div>
+
+      <p>React Monolithic Frontend for Deployment Practice</p>
+    </div>
+  );
+}
+
+export default App;
